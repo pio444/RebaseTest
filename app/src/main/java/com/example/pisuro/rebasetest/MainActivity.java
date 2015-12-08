692e51195da5778559774aea9446caa929aff7a8
Piotr Surówka <piotr.surowka@softiti.com>
2015-12-08 08:54:21 +0100

    Merged second and third and fourth commit into one!

--- app/src/main/java/com/example/pisuro/rebasetest/MainActivity.java
+++ app/src/main/java/com/example/pisuro/rebasetest/MainActivity.java
@@ -7,6 +7,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    //jsjsjs jsjsjsj jsjsjs
+    //sda
+    //new comment
+    //jakis komentarzasdas
+    //nowa linia w 4 komicie !!!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
